c9f2cb2e4f8f63ae26b2dfe54988bb7747a39850
julianooselame <julianooselame@hotmail.com>
2021-10-29 03:31:46 -0300

    Finalizado o programa de Cadastro de Séries

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -8,17 +8,19 @@ namespace DIO.Series
         public Serie(Genero genero, string titulo, string descricao) 
         {
             this.Genero = genero;
-                this.Titulo = titulo;
-                this.Descricao = descricao;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
                
         }
-                private Genero Genero { get; set; }
+        private Genero Genero { get; set; }
 
         private string Titulo { get; set; }
 
         private string Descricao { get; set; }
 
         private int Ano { get; set ; }
+
+        private bool Excluido { get; set; }
 
         //Métodos
 
@@ -29,6 +31,7 @@ namespace DIO.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -38,7 +41,8 @@ namespace DIO.Series
             retorno += "Genero: " + this.Genero + Environment.NewLine;
             retorno += "Título: " + this.Titulo + Environment.NewLine;
             retorno += "Descrição: " + this.Descricao + Environment.NewLine;
-            retorno += "Ano de Produção: " + this.Ano;
+            retorno += "Ano de Produção: " + this.Ano + Environment.NewLine;
+            retorno += "Excluído: " + this.Excluido;
 
             return retorno;
         }
@@ -51,6 +55,16 @@ namespace DIO.Series
         public int retornaId()
         {
             return this.Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+
+        public void Exclui()
+        {
+            this.Excluido = true;
         }
     }
 }
